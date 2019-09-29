59e6ffde0da652fe4cce40e00630b94960ccc99e
markdavidmc0 <mark@dotmodus.com>
2019-09-29 14:39:08 +0200

    # updated model to handle meta system contained within GCS.

--- recognet/recognition_api/models.py
+++ recognet/recognition_api/models.py
@@ -1,26 +1,39 @@
 from django.db import models
+from django.db.models.functions import Concat
 from django.utils import timezone
 
 
+unprocessed_video_url_env = 'https://console.cloud.google.com/storage/browser/' \
+                            'recognet-unprocessed-videos/'
+processed_video_url_env = 'https://console.cloud.google.com/storage/browser/' \
+                          'recognet-processed-videos/'
+unprocessed_image_url_env = 'https://console.cloud.google.com/storage/browser/' \
+                            'recognet-unprocessed-images/'
+processed_image_url_env = 'https://console.cloud.google.com/storage/browser/' \
+                            'recognet-processed-images/'
+
+
 class Person(models.Model):
-    """Image Recognition person security profile."""
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    age = models.IntegerField(default=0)
-    gender = models.CharField(max_length=30, choices=[('male', 'female')])
-    unprocessed_video_url = models.URLField(default='https://console.cloud.google.com/'
-                                                    'storage/browser/'
-                                                    'recognet-unprocessed-videos/')
-    processed_video_url = models.URLField(default='https://console.cloud.google.com/'
-                                                  'storage/browser/'
-                                                  'recognet-processed-videos/')
-    unprocessed_image_url = models.URLField(default='https://console.cloud.google.com/'
-                                                    'storage/browser/'
-                                                    'recognet-unprocessed-images/')
-    processed_image_url = models.URLField(default='https://console.cloud.google.com/'
-                                                  'storage/browser/'
-                                                  'recognet-processed-images/')
-    image = models.ImageField(upload_to='')
+    """Image recognition person security profile."""
+    first_name = models.CharField(max_length=30, default='unknown')
+    last_name = models.CharField(max_length=30, default='unknown')
+    year_of_birth = models.IntegerField(default=0)
+    location = models.CharField(max_length=30, default='unknown')
+    gender = models.CharField(max_length=30, choices=[('male', 'female')],
+                              default='unknown')
+    person_unprocessed_video_url = Concat(unprocessed_video_url_env, first_name, '_',
+                                          last_name, '_', year_of_birth, '_', location)
+    person_processed_video_url = Concat(processed_video_url_env, first_name, '_',
+                                        last_name, '_', year_of_birth, '_', location)
+    person_unprocessed_image_url = Concat(unprocessed_image_url_env, first_name, '_',
+                                          last_name, '_', year_of_birth, '_', location)
+    person_processed_image_url = Concat(processed_video_url_env, first_name, '_',
+                                        last_name, '_', year_of_birth, '_', location)
+
+    unprocessed_video_url = models.URLField(default=unprocessed_video_url_env)
+    processed_video_url = models.URLField(default=processed_video_url_env)
+    unprocessed_image_url = models.URLField(default=unprocessed_image_url_env)
+    processed_image_url = models.URLField(default=processed_image_url_env)
     modified = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(editable=False)
 

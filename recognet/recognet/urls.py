64cc7064bfb194223048ea45d776a5e715f10890
markdavidmc0 <mark@dotmodus.com>
2019-10-06 15:07:05 +0200

    Files/processing all running correctly. Added unittest with mock.

--- recognet/recognet/urls.py
+++ recognet/recognet/urls.py
@@ -19,7 +19,7 @@ from rest_framework import routers
 from recognition_api import views
 
 router = routers.DefaultRouter()
-router.register(r'person', views.PredictionViewSet)
+router.register(r'person', views.PersonViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- recognet/recognition_api/tests.py
+++ recognet/recognition_api/tests.py
@@ -1,44 +1,21 @@
-from io import BytesIO
-from PIL import Image
+from unittest import mock
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
-from rest_framework import viewsets
-
-from recognition_api.views import PredictionViewSet
 
 
 class PersonTestCase(APITestCase):
     """Test class for the Person Model."""
     factory = APIRequestFactory()
 
-    def test_person(self):
-        """Person."""
+    @mock.patch('recognition_api.views.PersonViewSet.upload_to_gcs')
+    def test_viewset(self, mock_upload):
+        """Test successful file pre-processing and viewset runs without error."""
+        mock_upload.return_value = 'test_blob'
         url = reverse('person-predict')
-        image = BytesIO()
         with open("/Users/markmc/Repos/RecogNet/recognet/recognition_api/test_files/"
-                  "mark_03.jpg", 'rb') as file:
-            image.write(file.read())
-            image.name = 'test_file.jpg'
-        # uploaded_file = InMemoryUploadedFile(
-        #     file=memory_file,
-        #     field_name='photo',
-        #     name='test.jpg',
-        #     content_type='application/json',
-        #     size=None,
-        #     charset=None,
-        # )
-
-        data = {'first_name': 'Mark', 'last_name': 'Mc Naught', 'image': image}
-        response = self.client.post(url, data)
-        # request = self.factory.post(
-        #     '/predict/', {'first_name': 'Mark'},
-        #     content_type='application/json'
-        # )
-
-        # request.FILES['image'] = uploaded_file
-        # response = PredictionViewSet.create(PredictionViewSet(), request)
-        x = 5
+                  "mark_07.jpg", 'rb') as image:
+            data = {'first_name': 'Mark', 'last_name': 'Mc Naught', 'image': image}
+            response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)

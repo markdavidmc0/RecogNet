a3baf1a938116f7a27d3f4c93bd2aac2aff51a63
markdavidmc0 <mark@dotmodus.com>
2019-11-16 21:29:18 +0200

    # changed import for ease of install.

--- recognet/recognition_api/helpers/fr_utils.py
+++ recognet/recognition_api/helpers/fr_utils.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 import numpy as np
 import os
-import cv2
+from cv2 import imread
 from numpy import genfromtxt
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
@@ -199,7 +199,7 @@ def load_dataset():
 
 
 def img_to_encoding(image_path, model):
-    img1 = cv2.imread(image_path, 1)
+    img1 = imread(image_path, 1)
     img = img1[..., ::-1]
     img = np.around(np.transpose(img, (2, 0, 1))/255.0, decimals=12)
     x_train = np.array([img])
